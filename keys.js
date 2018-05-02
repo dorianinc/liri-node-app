bab74bcb96f6cdbcb222a0c44fc583aafe333e28
Dorian Macias <dorianrdba@gmail.com>
2018-05-01 22:12:15 -0700

    .

--- keys.js
+++ keys.js
@@ -1,25 +1,26 @@
-// exports.twitter = {
-//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-// };
-
-// exports.spotify = {
-//   id: process.env.SPOTIFY_ID,
-//   secret: process.env.SPOTIFY_SECRET
-// };
-
 
 exports.twitter = {
-    consumer_key: 'hLOvCwkoo8DoHhpDJJ8lCIUBa',
-    consumer_secret: 'm1jb159YnmV55npAnyZXBm6TVsKKNqrZ0ChtkaRwTTrF9xblVI',
-    access_token_key: '205179243-kV4UEktDw5xikE2roPVeXS2rU6bjZattd3tK0Ukk',
-    access_token_secret: 'By8NCxmOLtMbrveOvloFr2AHdfOBThVcyqAzjmrFdRKWO'
-  }
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+
+// exports.twitter = {
+//     consumer_key: 'hLOvCwkoo8DoHhpDJJ8lCIUBa',
+//     consumer_secret: 'm1jb159YnmV55npAnyZXBm6TVsKKNqrZ0ChtkaRwTTrF9xblVI',
+//     access_token_key: '205179243-kV4UEktDw5xikE2roPVeXS2rU6bjZattd3tK0Ukk',
+//     access_token_secret: 'By8NCxmOLtMbrveOvloFr2AHdfOBThVcyqAzjmrFdRKWO'
+//   }
   
-  exports.spotify = {
-    id: 'f27e5364bcb048afb2bb7f779e76678d',
-    secret: '8ffce4e97aa04e27b244e1e2dd444cb5',
-  }
+//   exports.spotify = {
+//     id: 'f27e5364bcb048afb2bb7f779e76678d',
+//     secret: '8ffce4e97aa04e27b244e1e2dd444cb5',
+//   }
   
